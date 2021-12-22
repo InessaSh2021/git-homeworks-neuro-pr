2d20928379538cba3186962cdfe45ee1e48ae595
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2021-12-22 12:19:08 +0300

    Update hw.file.1.py

--- hw.file.1.py
+++ hw.file.1.py
@@ -1,8 +1,8 @@
 from pprint import pprint
 
-def get_dicht('cook_book_file.txt'):  
+def get_dict('cook_book_file.txt'):  
     with open('cook_book_file.txt') as file:
-        cook_book = dicht()
+        cook_book = dict()
         for line in file:
             dishes = line.strip()
             counter = int(file.readline())
@@ -17,4 +17,4 @@ def get_dicht('cook_book_file.txt'):
             cook_book[dishes] = temp_list
             file.readline()
         return cook_book
-pprint(get_data('cook_book_file.txt'))
+pprint(get_dict('cook_book_file.txt'))
