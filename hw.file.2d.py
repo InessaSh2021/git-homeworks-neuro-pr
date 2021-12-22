fb58a831f27963a6ab24e059531c13d6ebd69a03
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2021-12-22 22:32:33 +0300

    Update hw.file.2d.py

--- hw.file.2d.py
+++ hw.file.2d.py
@@ -1,12 +1,12 @@
 from pprint import pprint
 
 def get_shop_list_by_dishes(dishes_list, person_count):
-    cook_book_list = dict()
-    for dish in dishes_list:
-        if dish not in cook_book:
-            print('Такого блюда нет')
-        else:
-            for ingr in cook_book[dish]:
-                cook_book_list[ingr['ingr_name']]['quantity'] += int(ingr['quantity']) * person_count
-   return cook_book_list
-pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))  
+  cook_book = dict()
+  for dish in dishes_list:
+    if dish not in cook_book:
+      print('Такого блюда нет')
+    else:
+      for ingr in cook_book[dish]:
+        cook_book[ingr['ingr_name']]['quantity'] += int(ingr['quantity']) * person_count
+  return cook_book
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
